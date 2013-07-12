81736503d3ca2b47bfafe2d941f3ca6fc7d8bfcb
Eric Mensah <antwidua@gmail.com>
2013-07-12 13:20:42 +0000

    web.js file modified

--- web.js
+++ web.js
@@ -4,9 +4,29 @@ var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
   //response.send('Hello World2!');
-   response.send(fs.readFileSync('~/index.html'));
+fs.readFile('/index','utf8',function callBack(err,content){
+
+   if(err) return handleError(err);
+   response.send(content);
+
+});
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
   console.log("Listening on " + port);
 });
+});
+
+/**
+function callBack(err, content){
+
+  if(err){
+    return handError(err)
+}
+  else {
+      
+response.send(content);
+}
+
+
+} */
