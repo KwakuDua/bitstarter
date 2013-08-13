f93586aac650077da0c24848b1afd49bf3000450
Eric Mensah <antwidua@gmail.com>
2013-08-13 08:25:33 +0000

    Updated product description

--- web.js
+++ web.js
@@ -20,7 +20,7 @@ response.send(content);
 
 });
 
-var port = process.env.PORT || 8000;
+var port = process.env.PORT || 8080;
 app.listen(port, function() {
   console.log("Listening on " + port);
 });
